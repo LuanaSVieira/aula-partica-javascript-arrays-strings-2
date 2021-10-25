a488d172a859939a104196cea55b17d7f5e4dcb8
Luana Souza <87820972+LuanaSVieira@users.noreply.github.com>
2021-10-25 05:24:11 -0300

    Add files via upload

---
+++ atividade2.js
@@ -0,0 +1,18 @@
+// 1)
+
+var tabuada = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+ console.log(tabuada.join())
+
+// 2)
+
+var temp = [54, 78, 56, 98, 0, 12, 11, 37];
+var resultado1 = (54 - 32) / 1.8;
+var resultado2 = (78 - 32) / 1.8;
+var resultado3 = (56 - 32) / 1.8;
+var resultado4 = (98 - 32) / 1.8;
+var resultado5 = (0 - 32) / 1.8;
+var resultado6 = (12 - 32) / 1.8;
+var resultado7 = (11 - 32) / 1.8;
+var resultado8 = (37 - 32) / 1.8;
+
+console.log(resultado1, resultado2, resultado3, resultado4, resultado5, resultado6, resultado7, resultado8);
